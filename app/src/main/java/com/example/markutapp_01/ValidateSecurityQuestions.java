3a757e6e5b20291efbec74c3a5c0247a53973269
TwelveWings <samuel@twistedonyx.com>
2021-04-19 02:03:32 -0500

    Updated Files
    Updated AndroidManifest.xml to to allow screens to SecurityQuestion page and ChangePassword page to display.
    
    Updated ValidateSecurityQuestions.java to include a dropdown to select between questions.
    
    Added spinner to and removed hit from activity_answer_security_questions.xml

--- app/src/main/java/com/example/markutapp_01/ValidateSecurityQuestions.java
+++ app/src/main/java/com/example/markutapp_01/ValidateSecurityQuestions.java
@@ -3,15 +3,19 @@ package com.example.markutapp_01;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.List;
 
 public class ValidateSecurityQuestions extends AppCompatActivity {
 
@@ -44,11 +48,22 @@ public class ValidateSecurityQuestions extends AppCompatActivity {
 
     public void displaySecurityQuestions()
     {
+        Spinner questionList = (Spinner) findViewById(R.id.securityQuestions);
+
         String[] questions = getSecurityQuestions();
+        List<String> spinnerQuestions = new ArrayList<String>();
 
-        TextView q1 = (TextView)findViewById(R.id.question1);
+        for(int i = 0; i < questions.length; i++)
+        {
+            spinnerQuestions.add(questions[i]);
+        }
 
-        q1.setHint(questions[0]);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, questions);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        questionList.setAdapter(dataAdapter);
     }
 
     public void submitAnswers(View view)
@@ -65,17 +80,18 @@ public class ValidateSecurityQuestions extends AppCompatActivity {
 
         Intent intent = new Intent(getApplicationContext(), ChangePassword.class);
         startActivity(intent);
-
     }
 
     public boolean validateSecurityQuestions()
     {
         boolean isCorrectAnswer = false;
 
-        String[] questions = getSecurityQuestions();
+        Spinner uiQuestions = (Spinner)findViewById(R.id.securityQuestions);
         Dictionary answers = getAnswers();
 
-        String question = questions[0];
+        int questionID = uiQuestions.getSelectedItemPosition();
+        String[] questions = getSecurityQuestions();
+        String question = questions[questionID];
         String answer = (String)answers.get(question);
 
         EditText submittedAnswer = findViewById(R.id.question1);
